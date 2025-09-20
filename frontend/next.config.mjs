53f0af4721cfdf37bbc2f9c436ce054c2148a180
Edward-H26 <qiranhu8@gmail.com>
2025-09-20 00:42:17 -0500

    fix display bug

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/project-check-point-1-aidee' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/project-check-point-1-NOODEIA' : '',
   eslint: {
     ignoreDuringBuilds: true,
   },
